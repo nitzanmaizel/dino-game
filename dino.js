c14449e38235a99ab5195db6bfba7bc60bff7517
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-06-23 22:33:04 +0300

    dino.js - handleRun - if dino jumping return, else chnage between 0 and 1 and chnge the dimo image src

--- dino.js
+++ dino.js
@@ -11,13 +11,27 @@ let dinoFrame;
 let currentFrameTime;
 let yVelocity;
 
-export function setupDino() {}
+export function setupDino() {
+  isJumping = false;
+}
 
 export function updateDino(delta, speedScale) {
   handleRun(delta, speedScale);
   handleJump(delta);
 }
 
-function handleRun() {}
+function handleRun() {
+  if (isJumping) {
+    dinoElem.src = `imgs/dino-stationary.png`;
+    return;
+  }
+
+  if (currentFrameTime >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT;
+    dinoElem.src = `imgs/dino-run-${dinoFrame}.png`;
+    currentFrameTime -= FRAME_TIME;
+  }
+  currentFrameTime += delta * speedScale;
+}
 
 function handleJump() {}
