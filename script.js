edba6bc4128b42b04f8d9381db9d7032b8b6d386
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-06-23 20:55:30 +0300

    script - get world, score and start screen elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
+
+console.log({ worldElem, scoreElem, startScreenElem });
