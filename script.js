b79bd7f72242d8ac06ea7a50f6d3e36f1e7781e3
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-06-23 20:58:07 +0300

    script - create a func that set the wold elemn dimenssions by screen width and height

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
 const worldElem = document.querySelector("[data-world]");
 const scoreElem = document.querySelector("[data-score]");
 const startScreenElem = document.querySelector("[data-start-screen]");
 
-console.log({ worldElem, scoreElem, startScreenElem });
+setPixelToWorldScale();
+
+window.addEventListener("resize", setPixelToWorldScale);
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+  }
+
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
